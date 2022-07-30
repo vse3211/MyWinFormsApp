958c88ec62a861e247ea5b6e7e2029bef8467841
vse3211 <52658840.m@gmail.com>
2022-07-30 19:33:56 +0600

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text; // Данная зависимость серого цвета, не используемая в коде
+using System.Threading.Tasks;
+using System.Windows.Forms; // Данная зависимость белого цвета используется в коде
+
+namespace MyWinFormsApp
+{
+    public partial class Form1 : Form // : Form - наследование класса Form
+    {
+        public Form1()
+        {
+            // Инициализация формы. Находится в Form1.Designer.cs файле
+            InitializeComponent();
+            button1.Text = 0.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = (Convert.ToInt32(((Button)sender).Text) + 1).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Зависимости используемые в текущем файле
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Пространство имен приложения. Содержит все его классы
+namespace MyWinFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Включение визуальных стилей
+            Application.EnableVisualStyles();
+            // Установка стандартных значений (в данном случае false - отключено)
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Запуск приложения
+            Application.Run(new Form1());
+        }
+    }
+}
